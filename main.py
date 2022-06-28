01b4fcce9a4c355014f5683ff433abd9593aa7e7
Dmitry Sidorov <53134123+baritone1970@users.noreply.github.com>
2022-06-29 00:10:12 +0300

    deleted token

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-# Done! Congratulations on your new bot. You will find it at t.me/skillfactory202206281845_bot.
-# You can now add a description, about section and profile picture for your bot, see /help for a list of commands.
-# By the way, when you've finished creating your cool bot, ping our Bot Support if you want a better username for it.
-# Just make sure the bot is fully operational before you do this.
-#
-# Use this token to access the HTTP API:
-# 5266205613:AAGYbhEWn-9K51x_BGUjWSb48usEquJ1Ut0
-# Keep your token secure and store it safely, it can be used by anyone to control your bot.
-#
-# For a description of the Bot API, see this page: https://core.telegram.org/bots/api
-
 import telebot
 from extensions import APIException, Convertor
 from config import TOKEN, curencies_list
